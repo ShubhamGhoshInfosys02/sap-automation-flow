6043d88f948494b21271472983c05549dc982097
Shubham Ghosh <shubham.ghosh02@ad.infosys.com>
2025-01-13 13:17:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import ReactFlowUI from "./components/ReactFlowUI/ReactFlowUI";
+
+function App() {
+  return <ReactFlowUI />;
+}
+
+export default App;
---
+++ src/constants/initialConst.js
@@ -0,0 +1,21 @@
+export const initialNodes = [
+  // {
+  //   id: "3",
+  //   data: { label: "Stop" },
+  //   position: { x: 0, y: 0 },
+  //   type: "input",
+  // },
+  // {
+  //   id: "1",
+  //   data: { label: "Hello" },
+  //   position: { x: 0, y: 0 },
+  //   type: "input",
+  // },
+  // {
+  //   id: "2",
+  //   data: { label: "World" },
+  //   position: { x: 100, y: 100 },
+  // },
+];
+
+export const initialEdges = [];
